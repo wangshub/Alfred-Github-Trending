4530b6a4fe6c801f57a67748b5ace9a3802a7330
wangshub <easternslope@yeah.net>
2018-10-04 22:17:28 +0800

    feat: multithread req, check file exist

--- trending.py
+++ trending.py
@@ -1,31 +1,42 @@
 # -*- coding: utf-8 -*-
+import os
 import sys
+from multiprocessing.dummy import Pool as ThreadPool
 from workflow import Workflow, ICON_WEB, web
 
 DIR_AVATAR = './avatar/{author}.png'
+
+
+def get_avatar(author):
+    avatar_url = 'https://github.com/{author}.png?size=40'
+    avatar = web.get(avatar_url.format(author=author), allow_redirects=True)
+    avatar_path = DIR_AVATAR.format(author=author)
+    if not os.path.isfile(avatar_path):
+        with open(avatar_path, 'w') as f:
+            f.write(avatar.content)
+            # print('get {} done'.format(author))
 
 
 def get_trending_repos():
     language = 'all'
     since = 'daily'
     base_url = 'https://github-trending-api.now.sh/repositories?language={language}&since={since}'
-    avatar_url = 'https://github.com/{author}.png?size=40'
     req = web.get(base_url.format(language=language, since=since))
     req.raise_for_status()
     repos_json = req.json()
 
-    for repo in repos_json:
-        author = repo['author']
-        # avatar = web.get(avatar_url.format(author=author), allow_redirects=True)
-        # with open(DIR_AVATAR.format(author=author), 'w') as f:
-        #     f.write(avatar.content)
+    authors = [repo['author'] for repo in repos_json]
+    pool = ThreadPool(len(authors))
+    pool.map(get_avatar, authors)
+    pool.close()
+    pool.join()
 
     return repos_json
 
 
 def main(wf):
     # repos = get_trending_repos()
-    repos = wf.cached_data('posts', get_trending_repos, max_age=60 * 30)
+    repos = wf.cached_data('posts', get_trending_repos, max_age=1)
     for repo in repos:
         # print(repo['description'])
         subtitle = '{description}'.format(
